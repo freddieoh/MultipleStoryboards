8ca102ec43d7e35f5aa5ec48f40fe56b699dcd1a
Fredrick Ohen <fredrickohen@gmail.com>
2017-02-01 12:49:07 -0600

    First commit

---
+++ MultipleStoryboards/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  MultipleStoryboards
+//
+//  Created by Fredrick Ohen on 2/1/17.
+//  Copyright © 2017 geeoku. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  @IBAction func doneButtonPressed(_ sender: Any) {
+    
+    self.dismiss(animated: true, completion: nil)
+  }
+
+}
---
+++ MultipleStoryboards/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  MultipleStoryboards
+//
+//  Created by Fredrick Ohen on 2/1/17.
+//  Copyright © 2017 geeoku. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  
+  @IBAction func doneButtonPressed(_ sender: Any) {
+    
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+  
+}
